c6e91bdf4fb253098ce789e849f900def7df5245
David Hazel <dhazel@gmail.com>
2025-07-05 17:15:57 -0700

    feat(auth): login capable with both logto and kinde

--- app.vue
+++ app.vue
@@ -1,6 +1,24 @@
+
+<script setup lang="ts">
+  // import { useLogtoUser } from '#imports'; // Add this line if auto-import is disabled
+  const user = useLogtoUser();
+</script>
+
 <template>
   <div>
     <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <!-- Display user information when signed in -->
+    <ul v-if="Boolean(user)"> 
+      <li v-for="(value, key) in user"><b>{{ key }}:</b> {{ value }}</li>
+    </ul>
+    <!-- Simplified button for sign-in and sign-out -->
+    <a :href="`/sign-${ user ? 'out' : 'in' }`"> Sign {{ user ? 'out' : 'in' }} </a>
+
+    <NuxtLink to="/api/login" external>
+        Sign in
+    </NuxtLink>
+    <NuxtLink to="/api/register" external>
+        Sign up
+    </NuxtLink>
   </div>
 </template>
