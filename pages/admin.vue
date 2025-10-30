b3c938d70f7994ba0c8c4f715193cb675e6b560f
David Hazel <dhazel@gmail.com>
2025-10-29 21:18:20 -0700

    enh(ui): add an Admin page

---
+++ pages/admin.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h1>Admin Panel</h1>
+        <p>Administrative functions will be added here.</p>
+    </div>
+</template>
+
+<script setup>
+import { navigateTo, useNuxtApp } from '#app';
+
+const { $auth } = useNuxtApp();
+
+if (!$auth.loggedIn) {
+    navigateTo('/');
+}
+</script>
