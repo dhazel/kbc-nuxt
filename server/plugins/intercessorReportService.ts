a5392c189707ce571265aa37572ab5adce8d036a
David Hazel <dhazel@gmail.com>
2025-11-18 02:07:39 +0000

    enh(monday): add caching

--- server/plugins/intercessorReportService.ts
+++ server/plugins/intercessorReportService.ts
@@ -1,10 +1,13 @@
 import { SppInformedReportService } from '../services/IntercessorReport/SppInformedReportService';
 import { MondayAdapter } from '../adapters/MondayAdapter';
 import { MondayService } from '../services/Monday/MondayService';
+import { CachedMondayService } from '../services/Monday/CachedMondayService';
 
 export default defineNitroPlugin((nitroApp) => {
     nitroApp.hooks.hook('request', (event) => {
-        const mondayService = new MondayService(new MondayAdapter());
+        const mondayService = new CachedMondayService(
+            new MondayService(new MondayAdapter())
+        );
         event.context.intercessorReportService = new SppInformedReportService(mondayService);
     });
 });
---
+++ server/services/Monday/CachedMondayService.ts
@@ -0,0 +1,67 @@
+import {ActivityLog, IMondayService, ItemsResponse,} from "./IMondayService";
+
+export class CachedMondayService implements IMondayService {
+    constructor(private mondayService: IMondayService) {}
+
+    /**
+     * @param activityLogs - Collection of activity logs
+     * @returns Collection of all the items referenced by the given activity logs
+     */
+    public async getAllRelatedItems(activityLogs: ActivityLog[]): Promise<ItemsResponse> {
+        const cachedFunc = defineCachedFunction(async (activityLogs) => 
+            await this.mondayService.getAllRelatedItems(activityLogs),
+            {
+                maxAge: 10 * 60
+            });
+        return await cachedFunc(activityLogs);
+    }
+
+    /**
+     * @param activityLogs - Collection of activity logs
+     * @returns Collection of activity logs filtered to contain only those with a 'closed' status
+     */
+    public getClosedActivityLogs(activityLogs: ActivityLog[]) {
+        return this.mondayService.getClosedActivityLogs(activityLogs);
+    }
+
+    /**
+     * @param boards - Collection of board IDs
+     * @param endDate - End of the date range
+     * @param startDate - Start of the date range
+     * @returns Collection of all activity logs that had their status changed within the given date range
+     */
+    public async getAllStatusActivityLogs(
+        boards: number[],
+        startDate: Date,
+        endDate: Date
+    ): Promise<ActivityLog[]> {
+        const cachedFunc = defineCachedFunction(async (boards, startDate, endDate) => 
+            await this.mondayService.getAllStatusActivityLogs(boards, startDate, endDate),
+            {
+                maxAge: 10 * 60
+            });
+        return await cachedFunc(boards, startDate, endDate);
+    }
+
+    /**
+     * @param boardId - Id of a board on Monday.com
+     * @returns A map of status IDs to their label text
+     */
+    public async getStatusLabels(boardId: number): Promise<Record<number, string>> {
+        const cachedFunc = defineCachedFunction(async (boardId) => 
+            await this.mondayService.getStatusLabels(boardId),
+            {
+                maxAge: 60 * 60
+            });
+        return await cachedFunc(boardId);
+    }
+
+    public async getAllMondayUsers(): Promise<Record<string, string>> {
+        const cachedFunc = defineCachedFunction(async () => 
+            await this.mondayService.getAllMondayUsers(),
+            {
+                maxAge: 60 * 60
+            });
+        return await cachedFunc();
+    }
+}
