32701cebd98620faf0664ed67f05670a145cbce1
David Hazel <dhazel@gmail.com>
2025-12-04 01:39:37 +0000

    enh(reporting): add backoff, informed + inspired

--- server/plugins/intercessorReportService.ts
+++ server/plugins/intercessorReportService.ts
@@ -3,14 +3,17 @@ import { MondayAdapter } from '../adapters/MondayAdapter';
 import { MondayService } from '../services/Monday/MondayService';
 import { CachedMondayService } from '../services/Monday/CachedMondayService';
 import { AggregateIntercessorReportService } from '../services/IntercessorReport/AggregateIntercessorReportService';
+import { SppInspiredReportService } from '../services/IntercessorReport/SppInspiredReportService';
+import { MondayBackoffAdapter } from '../adapters/MondayBackoffAdapter';
 
 export default defineNitroPlugin((nitroApp) => {
     nitroApp.hooks.hook('request', (event) => {
         const mondayService = new CachedMondayService(
-            new MondayService(new MondayAdapter())
+            new MondayService(new MondayBackoffAdapter(new MondayAdapter()))
         );
         event.context.intercessorReportService = new AggregateIntercessorReportService([
-            new SppInformedReportService(mondayService)
+            new SppInformedReportService(mondayService),
+            new SppInspiredReportService(mondayService)
         ]);
     });
 });
