a93cd549eb13ac9c414211756da4f687317d8815
David Hazel <dhazel@gmail.com>
2026-01-12 04:20:45 +0000

    enh(db): add mondayId to models

---
+++ prisma/migrations/20260112041921_add_mondayid_for_syncing/migration.sql
@@ -0,0 +1,8 @@
+-- AlterTable
+ALTER TABLE "messages" ADD COLUMN     "mondayId" TEXT;
+
+-- AlterTable
+ALTER TABLE "prayer_orders" ADD COLUMN     "mondayId" TEXT;
+
+-- AlterTable
+ALTER TABLE "users" ADD COLUMN     "mondayId" TEXT;
