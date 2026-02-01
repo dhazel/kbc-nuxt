fa094c9774a6cdbcb5e4ba79a9bf7bcdeb3a5253
David Hazel <dhazel@gmail.com>
2026-02-01 23:41:49 +0000

    fix(auth): logout

--- app/pages/profile.vue
+++ app/pages/profile.vue
@@ -14,6 +14,6 @@ const permissionsStore = usePermissionsStore();
 
 const handleLogout = () => {
     permissionsStore.resetPermissions();
-    navigateTo('/api/logout');
+    window.location.href = '/api/logout';
 };
 </script>
